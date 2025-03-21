71d6bee2d26d72e54fc70cb47422563161eb42d5
MrASK2024 <askrasiukov@edu.hse.ru>
2025-03-21 16:29:55 +0300

    add doccker files

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ load_dotenv()
 EXCEL_FILE_PATH = os.getenv("EXCEL_FILE_PATH")
 
 if not EXCEL_FILE_PATH:
-    raise ValueError("Переменная окружения BOT_TOKEN не установлена")
+    raise ValueError("Переменная окружения EXCEL_FILE_PATH не установлена")
